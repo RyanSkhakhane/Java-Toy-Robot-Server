a0dd785e3a4623dc54bf3917927c96832878d616
cjohanne021 <cjohanne021@student.wethinkcode.co.za>
2022-04-13 13:45:39 +0200

    Client stuff

--- Robot_Client/src/main/java/za/co/wethinkcode/RobotClient/ClientCommands/ClientCommands.java
+++ Robot_Client/src/main/java/za/co/wethinkcode/RobotClient/ClientCommands/ClientCommands.java
@@ -1,6 +1,39 @@
 package za.co.wethinkcode.RobotClient.ClientCommands;
 
 public abstract class ClientCommands implements ClientCommandsInterface{
+    private final String name;
+    private String argument;
+    private String argument2;
+
+    public ClientCommands(String name){
+        this.name = name.trim().toLowerCase();
+        //this.argument="";
+    }
+
+    public ClientCommands(String name, String argument){
+        this(name);
+        this.argument=argument.trim();
+    }
+
+    public ClientCommands(String name, String argument, String argument2){
+        this(name);
+        this.argument=argument.trim();
+        this.argument2 = argument2.trim();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+
+
+    public String getArgument2() {
+        return argument2;
+    }
+
 
     public abstract String execute();
 
@@ -9,7 +42,7 @@ public abstract class ClientCommands implements ClientCommandsInterface{
         switch (args[0]) {
             case "launch":
                 System.out.println("Launch command passed");
-                return new Launch();
+                return new Launch(args[1], args[2]);
             case "look":
                 System.out.println("look command passed");
                 return new Look();
--- Robot_Client/src/main/java/za/co/wethinkcode/RobotClient/ClientCommands/Launch.java
+++ Robot_Client/src/main/java/za/co/wethinkcode/RobotClient/ClientCommands/Launch.java
@@ -1,8 +1,17 @@
 package za.co.wethinkcode.RobotClient.ClientCommands;
 
 public class Launch extends ClientCommands{
+
+    public Launch(String make, String name) {
+        super("launch",make,name);
+    }
+
     @Override
     public String execute() {
-        return "Launch normal cindy";
+        String commandName = getName();
+        String robotMake = getArgument();
+        String robotName = getArgument2();
+
+        return commandName + " " + robotMake + " " + robotName;
     }
 }
