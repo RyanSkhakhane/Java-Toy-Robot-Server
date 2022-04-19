8f6199797a5edb5e5dc7037f24cda7c50743738d
Thabisile Nsele <thabi@pop-os.localdomain>
2022-04-19 09:52:54 +0200

    g

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/World.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/World.java
@@ -5,6 +5,7 @@ import za.co.wethinkcode.robotServer.Position;
 import za.co.wethinkcode.robotServer.Robot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class World {
 
@@ -13,18 +14,29 @@ public class World {
     public static final Position CENTRE = new Position(0, 0);
     protected ArrayList<Obstacle> OBSTACLES;
     protected ArrayList<Robot> robots;
+    
+
+    public World (){this.OBSTACLES = makeObstacles();
+    }
 //    public static final Direction STARTDIRECTION = Direction.NORTH;
 
     public World(ArrayList<Robot> robotArrayList){
         this.OBSTACLES = makeObstacles();
         this.robots = robotArrayList;
     }
-
-
+    
     public ArrayList<Obstacle> makeObstacles(){
         ArrayList<Obstacle> obstacleList = new ArrayList<>();
         obstacleList.add(new SquareObstacle(-1,2));
         return obstacleList;
+    }
+    public void showObstacles() {
+        System.out.println("There are some obstacles");
+        for (int i = 0; i <= OBSTACLES.size() - 1; i++) {
+            System.out.println("- At position "+ OBSTACLES.get(i).getBottomLeftX()+","+OBSTACLES.get(i).getBottomLeftY()+"" +
+                    " (to "+(OBSTACLES.get(i).getBottomLeftX()+ 4)+","+(OBSTACLES.get(i).getBottomLeftY()+ 4)+")");
+
+        }
     }
 
     public ArrayList<Obstacle> getOBSTACLES(){
