99bd503ce68ed585b98382748acd3c5a11ab0d76
cjohanne021 <cjohanne021@student.wethinkcode.co.za>
2022-04-21 16:25:52 +0200

    Left, right and update direction

---
+++ Robot_Client/src/main/java/za/co/wethinkcode/RobotClient/ClientCommands/BackCommand.java
@@ -0,0 +1,15 @@
+package za.co.wethinkcode.RobotClient.ClientCommands;
+
+import za.co.wethinkcode.RobotClient.AdminCommands.Robots;
+
+public class BackCommand {
+    int steps;
+
+    public BackCommand(int steps) {
+        this.steps = steps;
+    }
+
+    public void execute(Robots robot) {
+
+    }
+}
