6d4d87a89c0cf7f34daa7f3760cbc955452a3135
cjohanne021 <cjohanne021@student.wethinkcode.co.za>
2022-04-21 16:17:18 +0200

    Left, right and update direction.

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/command/RightCommand.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/command/RightCommand.java
@@ -4,6 +4,7 @@ import za.co.wethinkcode.robotServer.Robot;
 
 public class RightCommand {
     public void execute(Robot robot){
+        //initial boolean is set to right
         robot.updateDirection(true);
     }
 }
