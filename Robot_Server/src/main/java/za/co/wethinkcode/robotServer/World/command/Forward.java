40f381cf50f6ae2b13e1d1c1d4c2a325d7ca100d
cstevens021 <cstevens021@student.wethinkcode.co.za>
2022-04-28 15:06:51 +0200

    small changes

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/command/Forward.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/World/command/Forward.java
@@ -1,8 +1,11 @@
 package za.co.wethinkcode.robotServer.World.command;
 
 import za.co.wethinkcode.robotServer.Robot;
+import za.co.wethinkcode.robotServer.ClientCommands;
 
-public class Forward {
+public class Forward extends ClientCommands{
+
+
 
     int steps;
 
