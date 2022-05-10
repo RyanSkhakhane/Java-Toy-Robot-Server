596877d8b2c447b16b1cce12678b3a10c352983c
Thabisile Nsele <thabi@pop-os.localdomain>
2022-05-10 09:20:52 +0200

    stash

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/ServerCommands/Quit.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/ServerCommands/Quit.java
@@ -4,6 +4,7 @@ import za.co.wethinkcode.robotServer.ClientHandler;
 import za.co.wethinkcode.robotServer.Robot;
 import za.co.wethinkcode.robotServer.World.World;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Quit extends ServerCommand{
@@ -11,6 +12,14 @@ public class Quit extends ServerCommand{
 
     @Override
     public void execute(ArrayList<ClientHandler> users, ArrayList<Robot> robots, World world){
+        for(ClientHandler user : users){
+            try {
+                user.getSocket().close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println("System shutting down");
         System.exit(0);
     }
