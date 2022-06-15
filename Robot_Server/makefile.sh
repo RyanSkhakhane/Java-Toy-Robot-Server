c93b06ee6865f9bea191d5d606b0f5ae9e8fed81
kmeyiswa021 <kmeyiswa021@student.wethinkcode.co.za>
2022-06-15 16:23:25 +0200

    Running all tests against our server (Q:Since our server requires you to enter port numbers and stuff, how do we solve that?) [15/06/2022]

--- Robot_Server/makefile.sh
+++ Robot_Server/makefile.sh
@@ -1,16 +1,26 @@
 
-
-
 #echo " "
-#echo ">>>>>>>>>>>>>>>>>>>>>>>>> V_E_R_I_F_Y_I_N_G :) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-#mvn verify
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>> R_U_N_N_I_N_G    R_E_F_E_R_E_N_C_E   S_E_R_V_E_R      <<<<<<<<<<<<<<<<<<<<<<<<"
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>>                    _&_                                <<<<<<<<<<<<<<<<<<<<<<<<"
+#echo ">>>>>>>>>>>>>>>>>>>>>>>>>                T_E_S_T_I_N_G                          <<<<<<<<<<<<<<<<<<<<<<<<"
+#echo  " "
+#java -jar ./src/libs/reference-server-0.1.0.jar &
+#
+#mvn test
+#
+#kill
+#
+#echo  " "
+#echo ">>>>>>>>>>>>>>>>>>>>  C_O_M_P_I_L_I_N_G :) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+#mvn compile
+
 
 echo " "
-echo ">>>>>>>>>>>>>>>>>>>>>>>>> R_U_N_N_I_N_G    R_E_F_E_R_E_N_C_E   S_E_R_V_E_R      <<<<<<<<<<<<<<<<<<<<<<<<"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>> R_U_N_N_I_N_G    O_U_R   S_E_R_V_E_R      <<<<<<<<<<<<<<<<<<<<<<<<"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>                    _&_                                <<<<<<<<<<<<<<<<<<<<<<<<"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>                T_E_S_T_I_N_G                          <<<<<<<<<<<<<<<<<<<<<<<<"
 echo  " "
-java -jar ./src/libs/reference-server-0.1.0.jar &
+java -jar ./target/Robot_Server-1.0-SNAPSHOT-jar-with-dependencies.jar &
 
 mvn test
 
