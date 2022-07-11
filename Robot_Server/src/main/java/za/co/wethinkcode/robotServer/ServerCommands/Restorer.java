ab868b60c6da80088e8e31fb00bd4af6109fb1d7
kmeyiswa021 <kmeyiswa021@student.wethinkcode.co.za>
2022-07-11 09:17:38 +0200

    Updated Restorer class

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/ServerCommands/Restorer.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/ServerCommands/Restorer.java
@@ -8,6 +8,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static za.co.wethinkcode.robotServer.DbConnect.IN_MEMORY_DB_URL;
 import static za.co.wethinkcode.robotServer.RobotServer.*;
 import static za.co.wethinkcode.robotServer.RobotServer.obstacleYCoord;
 
@@ -19,30 +20,38 @@ public class Restorer extends ServerCommand {
         Scanner in = new Scanner(System.in);
         String world_name=in.next();
 
+        String sql = "SELECT * FROM world_roboot WHERE world_name=?";
 
-        try(final Connection conn = connection.dbConnection;
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM world_roboot WHERE world_name=?")) {
-            System.out.println("SOUTE");
-            pstmt.setString(1, world_name);
+        try(final Connection conn = DriverManager.getConnection(IN_MEMORY_DB_URL)) {
+            System.out.println("Database connected!");
+
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+                System.out.println("World name: " + world_name);
+                pstmt.setString(1, world_name);
 //            stmt = connection.dbConnection.createStatement();
-            ResultSet rs = pstmt.executeQuery();
+                ResultSet rs = pstmt.executeQuery();
 //            System.out.println(rs);
 
-            System.out.println("OLDS: \nWord Size: " + worldSize + "\n Obstacle at (" + obsticleXCoord + "," + obstacleYCoord + ")");
+                System.out.println("OLDS: \nWord Size: " + worldSize + "\n Obstacle at (" + obsticleXCoord + "," + obstacleYCoord + ")");
 
-            while (rs.next()) { // something is wrong here
+                while (rs.next()) { // something is wrong here
 //                world_name = pstmt.get("world_name");
-                int robotWorldSize = rs.getInt("size");
-                int obstacle_x = rs.getInt("obstacles_x");
-                int obstacle_y = rs.getInt("obstacles_y");
-                System.out.println("Inside while loop");
-                worldSize = robotWorldSize + 1;
-                obstacleYCoord = obstacle_y + 1;
-                obsticleXCoord = obstacle_x + 1;
+                    int robotWorldSize = rs.getInt("size");
+                    int obstacle_x = rs.getInt("obstacles_x");
+                    int obstacle_y = rs.getInt("obstacles_y");
+                    System.out.println("Inside while loop");
+                    worldSize = robotWorldSize + 1;
+                    obstacleYCoord = obstacle_y + 1;
+                    obsticleXCoord = obstacle_x + 1;
+                }
+
+                System.out.println("NEWS: \nWord Size: " + worldSize + "\n Obstacle at (" + obsticleXCoord + "," + obstacleYCoord + ")");
+                System.out.println("Previous WORLD RESTORED successfully :)");
+
+            } catch (NullPointerException e){
+                System.out.println(e.getMessage());
             }
 
-            System.out.println("NEWS: \nWord Size: " + worldSize + "\n Obstacle at (" + obsticleXCoord + "," + obstacleYCoord + ")");
-            System.out.println("Previous WORLD RESTORED successfully :)");
         } catch (SQLException e) {
             System.out.println("HERE");
             System.out.println(e.getMessage());
