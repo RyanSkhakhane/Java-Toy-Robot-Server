b3634b3d787faed73d3a08745c948ffd60123d91
onetshim021 <onetshim021@student.wethinkcode.co.za>
2022-07-20 15:52:45 +0200

    doquit

--- Robot_Server/src/main/java/za/co/wethinkcode/robotServer/Api/HTTPServer.java
+++ Robot_Server/src/main/java/za/co/wethinkcode/robotServer/Api/HTTPServer.java
@@ -30,6 +30,7 @@ public class HTTPServer {
             path("/turn", HTTPServer::Turn);
             path("/unsupportedcommand", HTTPServer::UnsupportedCommand);
             path("/requestmessage", HTTPServer::RequestMessage);
+            path("/quit", HTTPServer::Quit);
         });
     }
 
@@ -140,6 +141,17 @@ public class HTTPServer {
     }
 
     private static void doRequestMessage(Context context) throws ClientCommands.CommandNotFoundException, FileNotFoundException {
+        ClientCommands clientCommand = ClientCommands.create(context.body());
+        ArrayList<Robot> robots = new ArrayList<>();
+        World world = new World(robots);
+        String message = clientCommand.execute(world, new String[]{"10"});
+        context.json(message);
+    }
+    public static void Quit() {
+        path("/", () -> get(HTTPServer::doQuit));
+    }
+
+    private static void doQuit(Context context) throws ClientCommands.CommandNotFoundException, FileNotFoundException {
         ClientCommands clientCommand = ClientCommands.create(context.body());
         ArrayList<Robot> robots = new ArrayList<>();
         World world = new World(robots);
